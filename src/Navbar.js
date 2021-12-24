73cded80e2308ec1c36fa6c32ae1a738e77d9c52
JJJ4032002 <rp4888888@gmail.com>
2021-12-24 22:36:51 +0530

    Added navbar styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 
+const NavStyles = styled.nav`
+  @import url("https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap");
+  padding: 0.6em;
+  background-color: #6c63ff;
+  color: white;
+  font-size: 1.3em;
+  font-weight: 700;
+  font-family: "Indie Flower";
+`;
 function Navbar() {
-  return <nav>Photo Tagging App</nav>;
+  return (
+    <NavStyles>
+      <h1>Photo Tagging App</h1>
+    </NavStyles>
+  );
 }
 
 export default Navbar;
