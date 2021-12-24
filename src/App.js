8609dd763075db1b648c92bf11d8520293de3e41
JJJ4032002 <rp4888888@gmail.com>
2021-12-24 22:35:51 +0530

    Added two components navbar and Main page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import Navbar from "./Navbar";
+import MainPage from "./MainPage";
+
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <MainPage></MainPage>
     </div>
   );
 }
