aedb7a1f181044f997a913681817f40469121466
JJJ4032002 <rp4888888@gmail.com>
2021-12-25 15:16:24 +0530

    Added responsiveness for MainPage and navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import { devices } from "./Media queries/Queries";
 
 const NavStyles = styled.nav`
   @import url("https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap");
   padding: 0.6em;
   background-color: #6c63ff;
+  display: flex;
+  align-items: center;
   color: white;
-  font-size: 1.3em;
+  font-size: clamp(1.2rem, 2.5vw, 1.5rem);
   font-weight: 700;
   font-family: "Indie Flower";
+  height: 8%;
+  @media ${devices.laptop} {
+    height: 12%;
+  }
 `;
 function Navbar() {
   return (
