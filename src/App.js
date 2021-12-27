6fb83ccc444aad93b7d8f16d16e541e3a6f84cdb
JJJ4032002 <rp4888888@gmail.com>
2021-12-27 14:43:15 +0530

    Added a use effect hook to check if we have selected an anchor tag element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import "./App.css";
-import Navbar from "./Navbar";
-import MainPage from "./MainPage";
+import Navbar from "./Components/Navbar";
+import MainPage from "./Components/MainPage";
+import Instructions from "./Components/Instructions";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
+  const { pathname, hash, key } = useLocation();
+  useEffect(() => {
+    // if not a hash link, scroll to top
+    if (hash === "") {
+      window.scrollTo(0, 0);
+    }
+    // else scroll to id
+    else {
+      setTimeout(() => {
+        const id = hash.replace("#", "");
+        const element = document.getElementById(id);
+        if (element) {
+          element.scrollIntoView();
+        }
+      }, 0);
+    }
+  }, [pathname, hash, key]); // do this on route change
   return (
     <div className="App">
       <Navbar></Navbar>
       <MainPage></MainPage>
+      <Instructions></Instructions>
     </div>
   );
 }
