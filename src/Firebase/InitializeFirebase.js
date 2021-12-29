1f00003c0f8d7aeff30ca213a8d6e6ea2cf8285f
JJJ4032002 <rp4888888@gmail.com>
2021-12-29 13:45:14 +0530

    Installed Firebase

---
+++ src/Firebase/InitializeFirebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjTz015sAbyzwj5aSBtVpW9n0Cyk-jxFw",
+
+  authDomain: "phototaggingapp-457c6.firebaseapp.com",
+
+  projectId: "phototaggingapp-457c6",
+
+  storageBucket: "phototaggingapp-457c6.appspot.com",
+
+  messagingSenderId: "71132886261",
+
+  appId: "1:71132886261:web:254fa381d44e71b33141bc",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
