465572221c1bc0fd90e226d7ceb6f01d955b5c3c
JJJ4032002 <rp4888888@gmail.com>
2022-01-07 22:33:16 +0530

    Added the styles in li

--- src/Components/Instructions.js
+++ src/Components/Instructions.js
@@ -18,6 +18,9 @@ const InstructionsDiv = styled.div`
   color: white;
   font-family: "Indie Flower";
 `;
+const ListItems = styled.li`
+  list-style-position: inside;
+`;
 
 function Instructions() {
   return (
@@ -25,11 +28,11 @@ function Instructions() {
       <InstructionsDiv>
         <h1>How to Play</h1>
         <ol>
-          <li>A list of characters will be displayed to you.</li>
-          <li>
+          <ListItems>A list of characters will be displayed to you.</ListItems>
+          <ListItems>
             You have to find those characters in a large picture in the minimal
             time to be placed higher on the leaderboard.
-          </li>
+          </ListItems>
         </ol>
       </InstructionsDiv>
     </FlexContainer>
