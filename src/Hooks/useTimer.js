cb91c92dd020962ad7fcd99e54ec3406d4f282d8
JJJ4032002 <rp4888888@gmail.com>
2022-01-10 21:00:10 +0530

    Added useTimer and useWindowSize hook

---
+++ src/Hooks/useTimer.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+function useTimer() {
+  const [time, setTime] = useState({
+    third: 0,
+    second: 0,
+    first: 0,
+  });
+  useEffect(() => {
+    const interval = setInterval(function () {
+      console.log("ran");
+
+      setTime((prev) => {
+        if (prev.second === 5 && prev.first === 9) {
+          return { third: prev.third + 1, second: 0, first: 0 };
+        } else if (prev.first === 9) {
+          return { ...prev, second: prev.second + 1, first: 0 };
+        } else {
+          return { ...prev, first: prev.first + 1 };
+        }
+      });
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return time;
+}
+
+export default useTimer;
