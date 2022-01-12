d17f3635fca2bef3af24654a609f5be133059bba
JJJ4032002 <rp4888888@gmail.com>
2022-01-12 23:54:32 +0530

    Added css of navbar in its own file

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,33 +1,8 @@
 import React from "react";
-import styled from "styled-components";
-import { devices } from "../Media queries/Queries";
+
 import { Link } from "react-router-dom";
 
-const NavStyles = styled.nav`
-  padding: 0.6em;
-  background-color: #6c63ff;
-  display: flex;
-  align-items: center;
-
-  color: white;
-  font-size: clamp(1.2rem, 2.5vw, 1.5rem);
-  font-weight: 700;
-  font-family: "Indie Flower";
-  height: 8%;
-  @media ${devices.laptop} {
-    height: 12%;
-    justify-content: space-around;
-  }
-`;
-const InstructionsHead = styled.h1`
-  cursor: pointer;
-  color: white;
-
-  display: none;
-  @media ${devices.laptop} {
-    display: block;
-  }
-`;
+import { InstructionsHead, NavStyles } from "./NavbarCss";
 
 function Navbar() {
   return (
