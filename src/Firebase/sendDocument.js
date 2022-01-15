abceb8529808c5a3138eab601d1682601721584c
JJJ4032002 <rp4888888@gmail.com>
2022-01-15 13:54:05 +0530

    Adding unique ids when adding documents in firestore

--- src/Firebase/sendDocument.js
+++ src/Firebase/sendDocument.js
@@ -1,11 +1,13 @@
 import { db } from "./InitializeFirebase";
 import { collection, addDoc } from "firebase/firestore";
+import { v4 as uuidv4 } from "uuid";
 
 async function sendDocument(name, timestamp) {
   // Add a new document with a generated id.
   const docRef = await addDoc(collection(db, "Level1"), {
     playerName: name,
     timeStamp: timestamp,
+    id: uuidv4(),
   });
 }
 export default sendDocument;
