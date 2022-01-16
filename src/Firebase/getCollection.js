179e42d8941507194e1c78152f71b6667c8fc49c
JJJ4032002 <rp4888888@gmail.com>
2022-01-16 21:14:54 +0530

    Updated getCollection and sentDocument

--- src/Firebase/getCollection.js
+++ src/Firebase/getCollection.js
@@ -1,12 +1,13 @@
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./InitializeFirebase";
-async function getCollection(DataFunction) {
+async function getCollection(DataFunction, Reminder) {
   const querySnapshot = await getDocs(collection(db, "Level1"));
   querySnapshot.forEach((doc) => {
     // doc.data() is never undefined for query doc snapshots
     console.log(doc.id, " => ", doc.data());
     DataFunction(doc.data());
   });
+  Reminder();
 }
 
 export default getCollection;
--- src/Firebase/sendDocument.js
+++ src/Firebase/sendDocument.js
@@ -1,13 +1,15 @@
 import { db } from "./InitializeFirebase";
 import { collection, addDoc } from "firebase/firestore";
 import { v4 as uuidv4 } from "uuid";
-
-async function sendDocument(name, timestamp) {
+import { serverTimestamp } from "firebase/firestore";
+async function sendDocument(name = "Anonymous", funcUser) {
   // Add a new document with a generated id.
   const docRef = await addDoc(collection(db, "Level1"), {
     playerName: name,
-    timeStamp: timestamp,
+
     id: uuidv4(),
+    startTimestamp: serverTimestamp(),
   });
+  funcUser(docRef.id);
 }
 export default sendDocument;
