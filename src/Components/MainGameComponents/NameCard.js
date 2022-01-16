8f811cf416bed4972e931d766fdadfda72fb3e5b
JJJ4032002 <rp4888888@gmail.com>
2022-01-16 21:17:25 +0530

    Added restart functionality in NameCard

--- src/Components/MainGameComponents/NameCard.js
+++ src/Components/MainGameComponents/NameCard.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 import { CardBody, NameCardBtn, Input, Heading, Form } from "./NameCardCss";
 
-function NameCard({ scale, getPlayerName }) {
+function NameCard({ scale, getPlayerName, Restart, BtnReference }) {
   const [currentVal, setCurrentVal] = useState("");
+
   function ChangeVal(e) {
     setCurrentVal((prev) => {
       return e.target.value;
@@ -15,6 +17,9 @@ function NameCard({ scale, getPlayerName }) {
       alert("Please enter your name");
     } else {
       getPlayerName(currentVal);
+      BtnReference.current.style["pointer-events"] = "none";
+      BtnReference.current.style["opacity"] = "0.5";
+      setCurrentVal("");
     }
   }
   return (
@@ -22,9 +27,13 @@ function NameCard({ scale, getPlayerName }) {
       <Heading>Please enter your name:</Heading>
       <Form>
         <Input onChange={ChangeVal} value={currentVal}></Input>
-        <NameCardBtn onClick={Submitted}>Submit</NameCardBtn>
-        <NameCardBtn>Restart</NameCardBtn>
-        <NameCardBtn>LeaderBoard</NameCardBtn>
+        <NameCardBtn ref={BtnReference} onClick={Submitted}>
+          Submit
+        </NameCardBtn>
+        <NameCardBtn onClick={Restart}>Restart</NameCardBtn>
+        <Link to="/leaderboard">
+          <NameCardBtn>LeaderBoard</NameCardBtn>
+        </Link>
       </Form>
     </CardBody>
   );
