f635ffe0762350758985d27c5d4ebd8f5da2b37f
JJJ4032002 <rp4888888@gmail.com>
2022-01-18 16:06:24 +0530

    Updated getCollection

--- src/Firebase/getCollection.js
+++ src/Firebase/getCollection.js
@@ -1,22 +1,18 @@
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./InitializeFirebase";
-async function calculateTime(data, func, id) {
-  let time = await data.endTimestamp.seconds;
-  let time2 = await data.startTimestamp.seconds;
-  if (data.playerName === "Anonymous") {
-    console.log("deleted");
-
-    func({ timestamp: time - time2, ...data, AnonId: id });
-  } else {
-    console.log("else");
-    func({ timestamp: time - time2, ...data });
-  }
-}
 
 async function getCollection(DataFunction, Reminder) {
   const querySnapshot = await getDocs(collection(db, "Level1"));
   querySnapshot.forEach((doc) => {
-    calculateTime(doc.data(), DataFunction, doc.id);
+    if (doc.data().playerName === "Anonymous") {
+      console.log("deleted");
+
+      DataFunction({ ...doc.data(), AnonId: doc.id });
+    } else {
+      console.log("else");
+      DataFunction({ ...doc.data() });
+    }
+
     // doc.data() is never undefined for query doc snapshots
   });
   console.log("bruh");
