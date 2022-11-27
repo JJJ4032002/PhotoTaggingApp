2f4df03b0741c37d73e2eae1ee186a0293c99465
JJJ4032002 <rp4888888@gmail.com>
2022-11-27 22:06:34 +0530

    Add framer motion

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,36 @@
-import React from "react";
-
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
 import { InstructionsHead, NavStyles } from "./NavbarCss";
-
+import { motion, useAnimation } from "framer-motion";
 function Navbar() {
+  const controls = useAnimation();
+  const [isAnimationPlaying, setIsAnimationPlaying] = useState(false);
   return (
     <NavStyles>
       <h1>Photo Tagging App</h1>
       <Link to="/#Inst">
-        <InstructionsHead>How to Play</InstructionsHead>
+        <InstructionsHead
+          as={motion.div}
+          onHoverStart={() => {
+            if (!isAnimationPlaying) {
+              setIsAnimationPlaying(true);
+              controls.start({
+                rotate: [0, 7, -7, 0],
+                transition: {
+                  duration: 0.4,
+                  ease: "easeInOut",
+                  repeatDelay: 1,
+                },
+              });
+            }
+          }}
+          animate={controls}
+          onAnimationComplete={() => {
+            setIsAnimationPlaying(false);
+          }}
+        >
+          How to Play
+        </InstructionsHead>
       </Link>
     </NavStyles>
   );
