6ada85126a434032c6b39ab455ba581a2ee5e338
JJJ4032002 <rp4888888@gmail.com>
2022-11-27 22:06:23 +0530

    Upgrade to react 18

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { createRoot } from "react-dom/client";
 
 import reportWebVitals from "./reportWebVitals";
-
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
